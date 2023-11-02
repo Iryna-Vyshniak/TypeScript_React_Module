3fa52a06e6fa18ca0b8c870d8775f82adddd95c5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-02 22:49:58 +0200

    Refactor: using useRef

--- src/components/NewTodoForm.tsx
+++ src/components/NewTodoForm.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 interface INewTodoForm {
-  value: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  // value: string;
+  // onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   // or
   // onChange: React.ChangeEventHandler<HTMLInputElement>;
-  handleClick: () => void;
+  // handleClick: () => void;
+  handleClick: (text: string) => void;
 }
 
-const NewTodoForm = ({ value, onChange, handleClick }: INewTodoForm) => {
+const NewTodoForm = ({ /* value, onChange, */ handleClick }: INewTodoForm) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const onClick = () => {
+    if (inputRef.current) {
+      if (!inputRef.current.value) return;
+      handleClick(inputRef.current.value);
+      inputRef.current.value = '';
+    }
+  };
+
   return (
-    <>
+    <div className="form">
       <input
         type="text"
         placeholder="new todo"
-        value={value}
-        onChange={onChange}
+        // value={value} // керована форма
+        // onChange={onChange}
+        ref={inputRef} // некерована форма
       />
-      <button onClick={handleClick}>Add Todo</button>
-    </>
+      {/* <button onClick={handleClick}>Add Todo</button> */}
+      <button onClick={onClick}>Add Todo</button>
+    </div>
   );
 };
 
